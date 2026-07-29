e47bd6f70d04ac41a3d53523e961d0ad6b43b9f2
JoseAmn <al45984@un.edu.mx>
2026-07-29 09:35:30 -0600

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+function operacionSumar() {
+    if(numero > numero2)
+}
+    var numero1 = 0;
+    var numero2 = 0;
+
+    numero1 = document.getElementById("numero1")
+    numero2 = document.getElementById("numero2")
